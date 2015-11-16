000a2e66f5b75f614eff1e8eb675e5f2f000de1d
kdex <kdex@kdex.de>
2015-11-16 15:21:28 +0100

    Added support for more module systems

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,17 +2,22 @@
 import gulp from "gulp";
 import babel from "gulp-babel";
 import uglify from "gulp-uglify";
+import merge from "merge2";
 process.env.FORCE_COLOR = true;
 gulp.task("js", () => {
-	return gulp.src("src/**/*.js")
-		.pipe(babel({
-			presets: ["es2015", "stage-0"],
-			plugins: ["transform-es2015-modules-systemjs"]
-		}))
-		.pipe(uglify({
-			mangle: true,
-		}))
-		.pipe(gulp.dest("dist"));
+	let streams = [];
+	for (let moduleType of ["amd", "commonjs", "systemjs", "umd"]) {
+		streams.push(gulp.src("src/**/*.js")
+			.pipe(babel({
+				presets: ["es2015", "stage-0"],
+				plugins: [`transform-es2015-modules-${moduleType}`]
+			}))
+			.pipe(uglify({
+				mangle: true
+			}))
+			.pipe(gulp.dest(`dist/${moduleType}`)));
+	}
+	return merge(streams);
 });
 gulp.task("default", () => {
 	gulp.start(["js"]);
