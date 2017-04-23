059cd1654363e73bd1b90653d9f4e19a26796c40
kdex <kdex@kdex.de>
2017-04-23 21:46:45 +0200

    Removed `gulp` and `uglify` from the project
    This was done in favor of Babili and npm scripts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,8 @@
 import gulp from "gulp";
 import babel from "gulp-babel";
-import uglify from "gulp-uglify";
 export function js() {
 	return gulp.src("src/**/*.js")
 		.pipe(babel())
-		.pipe(uglify({
-			mangle: true
-		}))
 		.pipe(gulp.dest("dist"));
 }
 export default gulp.parallel(js);
