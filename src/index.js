04f92a67778e89c33981a661de7c31620e3a877a
kdex <kdex@kdex.de>
2018-03-18 10:48:27 +0100

    Added tests, fixed minor issues
    Issue 1: Output strings were not in NFC normalization, which can be unexpected
    Issue 2: Superfluous whitespace wasn't taken into account in a few steps. This has been fixed.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,64 @@
-import statistics from "stats-lite";
+import { percentile } from "stats-lite";
 const categories = /[\p{Mn}\p{Me}]+/u;
-const DEFAULT_THRESHOLD = 0.5;
+const DEFAULT_THRESHOLD = 0.55;
+const compose = string => string.normalize("NFC");
+const decompose = string => string.normalize("NFD");
+/*
+* Computes a score ∈ [0, 1] for every word in the input string. Each score represents the ratio of Zalgo characters to total characters in a word.
+* @param {string} string
+* The input string for which to compute scores.
+* @return {number[]}
+* An array of scores where each score describes the Zalgo ratio of a word.
+*/
+export const computeScores = string => {
+	const wordScores = [];
+	/**
+	* Trimming here allows us to return early.
+	* Without trimming, we risk dividing by `0` later when computing the score.
+	*/
+	if (!string.trim().length) {
+		wordScores.push(0);
+	}
+	else {
+		for (const word of decompose(string).split(/\s+/)) {
+			let banned = 0;
+			for (const character of word) {
+				if (categories.test(character)) {
+					++banned;
+				}
+			}
+			const score = banned / word.length;
+			wordScores.push(score);
+		}
+	}
+	return wordScores;
+};
 /**
-* Determines if the string consists of Zalgo text. Note that the occurrence of a combining character is not enough to trigger this method to true. Instead, it assigns a score to each word in the string and applies some statistics to the total score. Thus, internationalized strings aren't automatically classified as Zalgo text.
-* @method isZalgo
-* @param {String} string
+* Determines if the string consists of Zalgo text. Note that the occurrence of a combining character is not enough to trigger this method to `true`. Instead, it computes a ratio for the input string and checks if it exceeds a given threshold. Thus, internationalized strings aren't automatically classified as Zalgo text.
+* @param {string} string
 * A string for which a Zalgo text check is run.
-* @param {Number} threshold
-* A threshold between 0 and 1. The higher the threshold, the more extreme Zalgo text cases are allowed.
-* @return {Boolean}
+* @param {number} threshold
+* A threshold ∈ [0, 1]. The higher the threshold, the more extreme Zalgo text cases are allowed.
+* @return {boolean}
 * Whether the string is a Zalgo text string.
 */
-export function isZalgo(string, threshold = DEFAULT_THRESHOLD) {
-	if (!string.length) {
-		return false;
-	}
-	const wordScores = [];
-	for (const word of string.normalize("NFD").split(" ")) {
-		/* Compute all categories */
-		let banned = 0;
-		for (const character of word) {
-			if (categories.test(character)) {
-				++banned;
-			}
-		}
-		const score = banned / word.length;
-		wordScores.push(score);
-	}
-	if (wordScores.length === 1) {
-		return wordScores[0] > threshold;
-	}
-	const totalScore = statistics.percentile(wordScores, 0.75);
-	return totalScore > threshold;
-}
+export const isZalgo = (string, threshold = DEFAULT_THRESHOLD) => {
+	const wordScores = computeScores(string);
+	const totalScore = percentile(wordScores, 0.75);
+	return totalScore >= threshold;
+};
 /**
 * Removes all Zalgo text characters for every word in a string if the word is Zalgo text.
-* @method clean
-* @param {String} string
+* @param {string} string
 * A string for which Zalgo text characters are removed for every word whose Zalgo property is met.
-* @param {Number} [threshold=0.5]
+* @param {number} [threshold=DEFAULT_THRESHOLD]
 * A threshold between 0 and 1. The higher the threshold, the more extreme Zalgo text cases are allowed.
-* @return {String}
+* @return {string}
 * A cleaned, readable string.
 */
-export function clean(string, threshold = DEFAULT_THRESHOLD) {
+export const clean = (string, threshold = DEFAULT_THRESHOLD) => {
 	let cleaned = "";
-	for (const word of string.normalize("NFD").split(/( )/)) {
+	for (const word of decompose(string).split(/(\s+)/)) {
 		if (isZalgo(word, threshold)) {
 			for (const character of word) {
 				if (!categories.test(character)) {
@@ -57,6 +70,6 @@ export function clean(string, threshold = DEFAULT_THRESHOLD) {
 			cleaned += word;
 		}
 	}
-	return cleaned;
-}
+	return compose(cleaned);
+};
 export default clean;
